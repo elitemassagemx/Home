e3568f622e093769f4abc129ef499cd07007efa2
elitemassagemx <elitemassagemx@gmail.com>
2024-08-27 20:46:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM loaded");
-    console.log("Services object:", services);
-    
-    const servicesList = document.getElementById('services-list');
-    console.log("Services list element:", servicesList);
-
     const services = {
         individual: [
     {
@@ -226,8 +219,13 @@ document.addEventListener('DOMContentLoaded', () => {
         
     };
 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM loaded");
+    console.log("Services object:", services);
 
     const servicesList = document.getElementById('services-list');
+    console.log("Services list element:", servicesList);
+
     const packageList = document.getElementById('package-list');
     const choiceChips = document.querySelectorAll('.choice-chip');
 
